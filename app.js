e510dae9bbd12317b09f60cd13afc4c41db54cd7
shrmhrjncode <shrmhrjn@gmail.com>
2026-09-23 17:05:28 +0800

    Add Docker build and automated tests

--- app.js
+++ app.js
@@ -4,5 +4,9 @@ const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+if (require.main === module) {
+    app.listen(port);
+    console.log(`App running on http://localhost:${port}`);
+}
+
+module.exports = app;
